090ac8edb19305f2114432198cdec4aaa002cd2e
Ian MacDonald & James Williams <student@epicodus.com>
2015-06-30 10:50:48 -0700

    Add next lesson method to lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,8 @@ class Lesson < ActiveRecord::Base
   validates :title, presence: true
   validates :lesson_number, presence: true
   validates :content, presence: true
+
+  def next
+    Lesson.find_by lesson_number: lesson_number + 1
+  end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 describe Lesson do
   it { should validate_presence_of :title }
+  context '#next' do
+    it "returns the lesson with the next highest number" do
+      current_lesson = Lesson.create(title: 'lesson1', content: "stuff", lesson_number: 1)
+      next_lesson = Lesson.create(title: 'lesson2', content: "stuff", lesson_number: 2)
+      expect(current_lesson.next).to eq next_lesson
+    end
+  end
 end
