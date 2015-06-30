debc11c9654dc21f3d63d1b6287702098855a077
Ian MacDonald & James Williams <student@epicodus.com>
2015-06-30 16:22:55 -0700

    Work on renaming uri path for table of contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "sections#index"
-  resources :lessons
   resources :sections
+  resources :lessons, except: [:index]
+  resources :lessons, as: :lessons, path: '/table_of_contents', only: :index
+
+  # match "/table_of_contents" => "lessons#index", via: :get
 end
