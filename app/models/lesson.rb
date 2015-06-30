1a2d689b0ec1dfbf97431a22d26cf3f48eebbb6f
Ian MacDonald & James Williams <student@epicodus.com>
2015-06-30 15:48:17 -0700

    add styling and order to the table of contents page

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,6 +5,9 @@ class Lesson < ActiveRecord::Base
   validates :content, presence: true
   belongs_to :section
 
+  default_scope { order(section_id: :desc, lesson_number: :asc)}
+
+
   def next
     Lesson.find_by lesson_number: lesson_number + 1
   end
