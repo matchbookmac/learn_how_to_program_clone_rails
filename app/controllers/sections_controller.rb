7be6b471dd57ede8722e2be0edf1c8ca1e02c647
Ian MacDonald & James Williams <student@epicodus.com>
2015-06-30 14:41:09 -0700

    Add edit and delete for section

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -22,6 +22,26 @@ class SectionsController < ApplicationController
     end
   end
 
+  def edit
+    @section = Section.find(params[:id])
+  end
+
+  def update
+    @section = Section.find(params[:id])
+    if @section.update(section_params)
+      flash[:notice] = "Section updated!"
+      redirect_to section_path(@section)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @section = Section.find(params[:id])
+    @section.destroy
+    redirect_to sections_path
+  end
+
 private
   def section_params
     params.require(:section).permit(:title)
